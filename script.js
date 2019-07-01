18f274a1bb2ba1961021a05483de73b8283c6014
Phu S Ly <tug03794@temple.edu>
2019-07-01 16:03:46 -0400

    portfolio added

---
+++ script.js
@@ -0,0 +1,37 @@
+(function(){
+
+    'use strict';
+
+
+    var $projects = $('.Project-Container');
+
+    $projects.isotope({
+        itemSelector: '.item'  
+    });
+
+    $('ul.filters > li').on('click', function(e){
+
+        e.preventDefault();
+
+        var filter = $(this).attr('data-filter');
+
+        $('ul.filters > li').removeClass('active');
+        $(this).addClass('active');
+
+        $projects.isotope({filter: filter});
+
+    });
+
+    $('.card').mouseenter(function(){
+
+        $(this).find('.card-overlay').css({'top': '-100%'});
+        $(this).find('.card-hover').css({'top':'0'});
+
+    }).mouseleave(function(){
+
+        $(this).find('.card-overlay').css({'top': '0'});
+        $(this).find('.card-hover').css({'top':'100%'});
+
+    });
+
+})(jQuery);
